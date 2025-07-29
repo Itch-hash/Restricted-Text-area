795f7bfdfe08e37da2ee3fa7bf253433a7211700
Itch-hash <contact.itch8@gmail.com>
2025-07-29 11:09:10 +0300

    made background change to red on max count

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ textInput.addEventListener("input", () => {
   textCount.innerHTML = `${textInput.value.length} / 250`;
   if (textInput.value.length >= 250) {
     textCount.style.color = "red";
+    textInput.style.borderColor = "red";
   } else {
     textCount.style.color = "initial";
+    textInput.style.borderColor = "initial";
     console.log(textInput.value.length);
   }
 });
